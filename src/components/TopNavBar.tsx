9112b8cde01049efec0a2d58a02472a5989133c9
Yachu Joshi <yachujoshi1234@gmail.com>
2022-05-24 08:23:15 +0545

    Minor changes :recycle:

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { NavIcons } from "./NavIcons";
+import { Container } from "./Container";
 import { RiSearchLine } from "react-icons/ri";
 
 import styles from "./TopNavBar.module.scss";
@@ -7,21 +8,23 @@ import styles from "./TopNavBar.module.scss";
 export const TopNavBar = () => {
   return (
     <header className={styles.Header}>
-      <nav className={styles.Nav}>
-        <figure className={styles.Figure}>
-          <Image
-            alt="Logo"
-            src="/ig_logo.svg"
-            layout="fill"
-            objectFit="contain"
-          />
-        </figure>
-        <div className={styles.InputContainer}>
-          <RiSearchLine className={styles.Icon} />
-          <input type="search" placeholder="Search" />
-        </div>
-        <NavIcons />
-      </nav>
+      <Container>
+        <nav className={styles.Nav}>
+          <figure className={styles.Figure}>
+            <Image
+              alt="Logo"
+              src="/ig_logo.svg"
+              layout="fill"
+              objectFit="contain"
+            />
+          </figure>
+          <div className={styles.InputContainer}>
+            <RiSearchLine className={styles.Icon} />
+            <input type="search" placeholder="Search" />
+          </div>
+          <NavIcons />
+        </nav>
+      </Container>
     </header>
   );
 };
