c9d28f3209c57c2c2a77552bba3706293e54ad35
Yachu Joshi <yachujoshi1234@gmail.com>
2022-05-27 11:09:41 +0545

    Minor refactor :recycle:

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { useState, useCallback, useContext, createContext } from "react";
 import { AxiosBasicCredentials, AxiosError } from "axios";
+import { useRouter } from "next/router";
 import { User } from "../types";
 import { parseJWT, notify } from "../utils";
 import { login as loginUser } from "../services";
@@ -10,6 +11,7 @@ interface Props {
 
 interface Context {
   user: User;
+  loading: boolean;
   login: ({ username, password }: AxiosBasicCredentials) => Promise<void>;
 }
 
@@ -19,14 +21,18 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: Props) => {
   const [user, setUser] = useState({} as User);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const login = useCallback(
     async ({ username, password }: AxiosBasicCredentials) => {
+      setLoading(true);
       try {
         const { data } = await loginUser({ username, password });
         setUser(parseJWT(data.accessToken));
         localStorage.setItem("accessToken", data.accessToken);
         localStorage.setItem("refreshToken", data.refreshToken);
+        router.push("/");
       } catch (e) {
         if (e instanceof AxiosError) {
           const responseError = e as AxiosError<string>;
@@ -34,13 +40,15 @@ export const AuthProvider = ({ children }: Props) => {
             return notify("error", responseError.response.data);
           }
         }
+      } finally {
+        setLoading(false);
       }
     },
-    []
+    [router]
   );
 
   return (
-    <AuthContext.Provider value={{ user, login }}>
+    <AuthContext.Provider value={{ user, loading, login }}>
       {children}
     </AuthContext.Provider>
   );
